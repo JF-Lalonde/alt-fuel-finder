14ee6c8cfe1b6f96c1590ceea94175347b4c5ac1
JF-Lalonde <lalondej88@gmail.com>
2017-08-30 10:45:23 -0600

    Debugging creating of a station using JSON data

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchController < ApplicationController
   def index
     conn = Faraday.get "https://api.data.gov/nrel/alt-fuel-stations/v1.json?fuel_type=LPG,ELEC&zip=#{params[:q]}&api_key=#{ENV['NREL_KEY']}"
-    @response = JSON.parse(conn.body, symbolize_names: true)
-    @stations = Station.create(@response)
+    response = JSON.parse(conn.body, symbolize_names: true)
+    Station.create!(response)
+    @stations = Station.all
   end
-
 end
--- app/models/station.rb
+++ app/models/station.rb
@@ -6,5 +6,6 @@ class Station < ActiveRecord::Base
       @fuel_types = station["fuel_type_code"]
       @distance = station["id"] #Need to go back and create a distance-finding method
       @access_time = station["access_days_time"]
+    end
   end
 end
