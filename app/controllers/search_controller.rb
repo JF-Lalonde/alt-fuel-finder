95a5528414bf02b80995cce15b570480a883517b
JF-Lalonde <lalondej88@gmail.com>
2017-08-30 10:10:17 -0600

    Successfully returns stations based on fuel type and zipcode

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,5 @@
 class SearchController < ApplicationController
   def index
-    @conn = Faraday.get "https://api.data.gov/nrel/alt-fuel-stations/v1.json?fuel_type=LPG,ELEC&zip=#{params[:q]}&limit=10&api_key=#{ENV['NREL_KEY']}"
-    byebug
+    @conn = Faraday.get "https://api.data.gov/nrel/alt-fuel-stations/v1.json?fuel_type=LPG,ELEC&zip=#{params[:q]}&api_key=#{ENV['NREL_KEY']}"
   end
 end
