ca6cc72d3f8ae8ab880d23ce03a0494d768168ff
JF-Lalonde <lalondej88@gmail.com>
2017-08-30 10:35:37 -0600

    Create station using API-returned values

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,8 @@
 class SearchController < ApplicationController
   def index
     conn = Faraday.get "https://api.data.gov/nrel/alt-fuel-stations/v1.json?fuel_type=LPG,ELEC&zip=#{params[:q]}&api_key=#{ENV['NREL_KEY']}"
-    @response = JSON.parse(conn.body)
+    @response = JSON.parse(conn.body, symbolize_names: true)
+    @stations = Station.create(@response)
   end
+
 end
