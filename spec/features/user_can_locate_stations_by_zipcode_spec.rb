84b8696678bf28a7c11f0ab23181ad259c5b14f5
JF-Lalonde <lalondej88@gmail.com>
2017-08-30 09:20:47 -0600

    Finish writing initial feature test. Will need to add station factory

--- spec/features/user_can_locate_stations_by_zipcode_spec.rb
+++ spec/features/user_can_locate_stations_by_zipcode_spec.rb
@@ -9,5 +9,12 @@ feature "User enters zipcode" do
     expect(current_path).to eq("/search")
     expect(params).to_not be_nil
     expect(page).to have_content("Station content")
+    expect(stations.count).to eq(10)
+    expect(station.type).to eq("Electric" || "Propane")
+    expect(page).to have_content(station.name)
+    expect(page).to have_content(station.address)
+    expect(page).to have_content(station.fuel_types)
+    expect(page).to have_content(station.distance)
+    expect(page).to have_content(station.access_time)
   end
 end
