8671c9467d405ab0b74babb77fefc368d8c5364c
JF-Lalonde <lalondej88@gmail.com>
2017-08-30 10:16:06 -0600

    Start building out station model using API data

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,6 @@
 class SearchController < ApplicationController
   def index
-    @conn = Faraday.get "https://api.data.gov/nrel/alt-fuel-stations/v1.json?fuel_type=LPG,ELEC&zip=#{params[:q]}&api_key=#{ENV['NREL_KEY']}"
+    conn = Faraday.get "https://api.data.gov/nrel/alt-fuel-stations/v1.json?fuel_type=LPG,ELEC&zip=#{params[:q]}&api_key=#{ENV['NREL_KEY']}"
+    @response = JSON.parse(conn.body)
   end
 end
